6e436666c444e2f5b91aa4463786479442b1bce2
maciejjanocinski <maciejjanocinski@gmail.com>
2023-07-28 21:11:18 +0200

    #1 added custom login page

--- src/main/java/app/models/UserEntity.java
+++ src/main/java/app/models/UserEntity.java
@@ -1,20 +1,18 @@
-package app.Entities;
+package app.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 
 
 @Data
 @Entity
+@Table(name = "users", uniqueConstraints = @UniqueConstraint(columnNames = "email") )
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String name;
+    private String username;
 
     private String email;
 
@@ -23,7 +21,7 @@ public class User {
     private String role;
 
     public User(String name, String email, String password) {
-        this.name = name;
+        this.username = name;
         this.email = email;
         this.password = password;
     }
