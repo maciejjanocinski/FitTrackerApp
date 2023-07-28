ad71795a6c715f737870d923f22a40884dfa8c48
maciejjanocinski <maciejjanocinski@gmail.com>
2023-07-28 14:16:54 +0200

    project initalization

---
+++ src/main/java/app/FitTrackerAppApplication.java
@@ -0,0 +1,13 @@
+package app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitTrackerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitTrackerAppApplication.class, args);
+	}
+
+}
