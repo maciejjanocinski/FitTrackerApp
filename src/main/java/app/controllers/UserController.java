bf7a5308956dcadc46fee0a8663556b33ac56629
maciejjanocinski <maciejjanocinski@gmail.com>
2023-07-28 23:02:00 +0200

    adding all files

--- src/main/java/app/controllers/UserController.java
+++ src/main/java/app/controllers/UserController.java
@@ -1,2 +1,15 @@
-package app.controllers;public class UserController {
+package app.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    @GetMapping("/")
+    public String user() {
+        return "user";
+    }
 }
